62c086de9541b4f97283d13e83d1189f73495b43
Yaren Ulusoy <200201128@kocaeli.edu.tr>
2023-07-12 18:50:04 +0300

    Proje dosyası ekle.

---
+++ WebApplication3/Model/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication3.Model
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string MoneyType { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
